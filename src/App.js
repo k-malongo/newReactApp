0e09efca3c01e1a369d5c54fe0f5323527bd255c
k-malongo <36006542+k-malongo@users.noreply.github.com>
2021-05-04 13:07:54 +0300

    updated comments

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import "./styles.css";
-import {Component} from 'react';
+import { Component } from "react";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super();
     this.state = {
-      buttonClicked : false,
-    }
+      buttonClicked: false
+    };
   }
 
-  
+  //declare fuction handleClick
+  handleClick = this.handleClick.bind(this);
 
-  //declare fuction
- handleClick = this.handleClick.bind(this);
-
-//define function
-  handleClick(){
-   this.setState({
-     buttonClicked: true,
-   })
+  //define function
+  handleClick() {
+    this.setState({
+      buttonClicked: true
+    });
   }
-  render(){
+  render() {
+    return (
+      <div className="App">
+        <h1 className="text">App</h1>
+        <div className="don">
+          <h2>Start editing to see some magic happen!</h2>
 
-  return (
-    <div className="App">
-      <h1 className="text">App</h1>
-      <div className="don">
-      <h2>Start editing to see some magic happen!</h2>
-      
-      <button onClick = {this.handleClick}> Y'all bitches</button>
-      <button> Y'all bitches</button>
-      <button> Y'all bitches</button>
-      
-       </div>
-       <div className="textBox">{this.state.buttonClicked ? <div> Button Clicked</div>: ""} </div>
-    </div>
-    
-  );
+          <button onClick={this.handleClick}> Y'all bitches</button>
+          <button> Y'all bitches</button>
+          <button> Y'all bitches</button>
+        </div>
+        <div className="textBox">
+          {this.state.buttonClicked ? <div> Button Clicked</div> : ""}{" "}
+        </div>
+      </div>
+    );
   }
 }
 
